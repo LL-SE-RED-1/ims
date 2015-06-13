74127c8933e74436916128768d2f9616b5fa37c1
tbabm <liuzxzju@gmail.com>
2015-06-13 14:07:52 +0800

    add comments

--- application/models/ims/user_model.php
+++ application/models/ims/user_model.php
@@ -3,30 +3,38 @@
 class User_model extends CI_Model {
 	public function __construct() {
 		parent::__construct();
+		//load database
 		$this->load->database();
 	}
 
+	//verify whether the user exists
 	public function verify_user($post) {
 		$where_array = array('uid' => $post['uid'],
 			'password' => md5($post['password']),
 			'type' => $post['userType']);
+		//search in database
 		$query = $this->db->get_where('imsUser', $where_array);
 
 		if ($query->num_rows == 1) {
+			//if exists
 			return TRUE;
 		} else {
+			//if not exists
 			return FALSE;
 		}
 
 	}
 
+	//modify user's password in database
 	public function modify_pass($post) {
 		$data = array(
        		'password' => md5($post['new_pass'])
     	);
 
+		//update data in database
 		$result = $this->db->update('imsUser', $data, array('uid' => $post['uid']));
 
+		//return the result of update
 		return $result;
 	}
 
